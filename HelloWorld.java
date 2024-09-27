900d76e6e81bc8e29ec8690ab1a86df6c7bc8abc
Setyo Dwi Gauharis <setydwigauharis10@gmail.com>
2024-09-27 09:34:32 +0700

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/* kutip 1('' -> untuk karakter), kutip 2("" -> untuk string) 
+    buat repositories di github
+    masukkan git init di terminal vs code
+*/
+
+public class HelloWorld{
+    public static void main(String [] args){
+        System.out.println("Hello World");
+    }
+}
